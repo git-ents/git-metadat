c4ed9f85753443685fbaa63b13ffb1501f4d92d1
Joseph D. Carpinelli <joseph.d.carpinelli@loopy.codes>
2026-03-08 19:23:24 -0400

    feat: add initial trait method and accompanying implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+///! Testingg
+use git2;
+use git2::{Error, Repository};
+
+#[derive(Debug)]
+pub struct MetadataOptions {
+    pub name: String,
+    pub shard_level: u8,
+    pub force: bool,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Default for MetadataOptions {
+    fn default() -> Self {
+        Self {
+            name: "refs/metadata/commits".to_string(),
+            shard_level: 4,
+            force: false,
+        }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub trait MetadataIndex<'a> {
+    fn add_metadata(
+        &'a self,
+        oid: &git2::Oid,
+        metadata: &git2::Tree,
+        maybe_commit: Option<&git2::Commit>,
+        maybe_opts: Option<&MetadataOptions>,
+    ) -> Result<git2::Reference<'a>, Error>;
+}
+
+impl<'a> MetadataIndex<'a> for Repository {
+    fn add_metadata(
+        &'a self,
+        oid: &git2::Oid,
+        metadata: &git2::Tree,
+        maybe_commit: Option<&git2::Commit>,
+        maybe_opts: Option<&MetadataOptions>,
+    ) -> Result<git2::Reference<'a>, Error> {
+        let commit = maybe_commit.unwrap_or(&self.head()?.peel_to_commit()?);
+
+        let default_opts = MetadataOptions::default();
+        let opts = maybe_opts.unwrap_or(&default_opts);
+
+        let reference = match self.find_reference(&opts.name) {
+            Ok(reference) => Some(reference),
+            Err(e) if e.code() == git2::ErrorCode::NotFound => None,
+            Err(e) => return Err(e),
+        };
+
+        todo!()
     }
 }
